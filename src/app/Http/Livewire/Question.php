013da90512025c9f4bdaccb4d6775763b9f79a89
Alexander Wulf <alexander.wulf@kefischer.de>
2020-09-10 16:37:11 +0200

    Some good Livewire progress

--- src/app/Http/Livewire/Question.php
+++ src/app/Http/Livewire/Question.php
@@ -4,18 +4,26 @@ namespace App\Http\Livewire;
 
 use App\Models\License;
 use App\Models\Task;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Question extends Component
 {
     public License $license;
     public Task $task;
+    public EloquentCollection $responses;
     public bool $showSolution = false;
+    public Collection $media;
     public $selected;
+
+    public array $alphabet = ["A", "B", "C", "D"];
 
     public function mount()
     {
-        $this->task = $this->getTask();
+        // $this->task = $this->getTask();
+        $this->task = Task::find(23);
+        $this->assignTaskMisc();
     }
 
     public function submit(){
@@ -29,6 +37,11 @@ class Question extends Component
     public function render()
     {
         return view('livewire.question');
+    }
+
+    private function assignTaskMisc(): void{
+        $this->responses = $this->task->responses->shuffle();
+        $this->media = $this->task->getMedia();
     }
 
     private function getTask(): Task
--- src/resources/views/livewire/question.blade.php
+++ src/resources/views/livewire/question.blade.php
@@ -1,10 +1,26 @@
-<div x-data="{selected: 0}">
-   <h2>{{ $task->question }}</h2>
-   @foreach ($task->responses->shuffle() as $item)
-   <div x-on:click="select = {{ $item->id }}" x-bind:class="{selected == {{ $item->id }} ? 'text-red' : ''}">
-       
-           {{ $item->text }}
-   </div>
-   @endforeach
-   {{ $selected }}
+<div x-data="{ selected: @entangle('selected')}" class="space-y-4">
+        <h2 class="text-lg font-medium">{{ $task->question }}</h2>
+        @isset ($media)
+        <div class="flex flex-row justify-center space-x-4">
+                @foreach ($media as $m)
+                <img src="{{ $m->getFullUrl() }}">
+                @endforeach
+        </div>
+        @endisset
+        @foreach ($responses as $item)
+        <div class="group">
+                <button type="button"
+                        class="flex flex-row items-center w-full p-4 text-left bg-white border-2 rounded-lg shadow hover:border-indigo-500 hover:shadow-lg"
+                        x-on:click="selected = {{ $item->id }}">
+                        <div class="px-2 py-1 mr-2 text-xs text-gray-500 border rounded-sm group-hover:bg-gray-100">
+                                {{ $alphabet[$loop->index] }}
+                        </div>
+                        <div>
+                                {{ $item->text }}
+                        </div>
+                </button>
+        </div>
+        @endforeach
+        <x-jet-button class="w-full">Frage beantworten</x-jet-button>
+        <span x-text="selected"></span>
 </div>
